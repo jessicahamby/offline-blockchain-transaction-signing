df6bbe3d5332a1f6c829420900fe035ef5e2cba3
realabbas <irizviali@gmail.com>
2022-05-13 21:43:54 +0530

    Remove redundant code

--- src/screens/qr/generate/index.js
+++ src/screens/qr/generate/index.js
@@ -1,37 +1,14 @@
 import { javascript } from '@codemirror/lang-javascript';
-import Typography from '@mui/material/Typography';
 import CodeMirror from '@uiw/react-codemirror';
 import QRCode from 'qrcode';
 // Importing qrcode-reader module
 import React from "react";
 import Input from '../../../components/input';
 import defaultTxParams from '../../../default/txParams';
-import decodeQR from '../../../utils/decodeQR';
 
 function App() {
 
     const [txParams, setTxParams] = React.useState(defaultTxParams)
-
-    const [fileName, setFileName] = React.useState([]);
-
-    const parseFileName = (files) => {
-        const ans = [];
-        for (let i = 0; i < Object.keys(files).length; i += 1) {
-            ans.push({ name: files[i].name });
-        }
-        setFileName(ans);
-    };
-
-    const showFile = async (e) => {
-        parseFileName(e.target.files);
-        e.preventDefault();
-        const reader = new FileReader();
-        reader.onload = async (e) => {
-            const fileBuffer = (e.target.result);
-            const temp = await decodeQR(fileBuffer)
-        }
-        reader.readAsArrayBuffer(e.target.files[0]);
-    };
 
     React.useEffect(() => {
         QRCode.toDataURL(JSON.stringify(txParams))
@@ -78,14 +55,6 @@ function App() {
                     <img id="qr" alt="qr" style={{ height: "400px", marginTop: 90 }} />
                 </div>
             </div>
-            <input
-                accept="image/apng, image/avif, image/gif, image/jpeg, image/png, image/svg+xml, image/webp"
-                type="file"
-                name="file-upload-input"
-                id="file-upload-input"
-                onChange={(e) => showFile(e)}
-            />
-            {fileName.length > 0 ? <Typography variant="subtitle1">{`${fileName[0].name}`}</Typography> : null}
 
         </div>
 
