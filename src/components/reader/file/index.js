cad6750bef6a3bd2367f282a31176b5bc8585115
realabbas <irizviali@gmail.com>
2022-05-13 18:21:35 +0530

    Restructure project, add device specific code, file upload feature

--- src/components/reader/file/index.js
+++ src/components/reader/file/index.js
@@ -1,30 +1,77 @@
-//Importing jimp module
-var Jimp = require("jimp");
-// Importing filesystem module
-var fs = require('fs')
-// Importing qrcode-reader module
-var qrCode = require('qrcode-reader');
-
-// Read the image and create a buffer
-// (Here image.png is our QR code)
-console.log("_", __dirname)
-var buffer = fs.readFileSync(__dirname + '/qr.png');
-console.log(typeof buffer)
-// Parse the image using Jimp.read() method
-Jimp.read(buffer, function (err, image) {
-    if (err) {
-        console.error(err);
-    }
-    // Creating an instance of qrcode-reader module
-    let qrcode = new qrCode();
-    qrcode.callback = function (err, value) {
-        if (err) {
-            console.error(err);
-        }
-        // Printing the decrypted value
-        console.log(value.result);
-        return value.result
-    };
-    // Decoding the QR code
-    qrcode.decode(image.bitmap);
+import { javascript } from '@codemirror/lang-javascript';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import CodeMirror from '@uiw/react-codemirror';
+import React from "react";
+import decodeQR from '../../../utils/decodeQR';
+const Input = styled('input')({
+    display: 'none',
 });
+function Read() {
+
+    const [fileName, setFileName] = React.useState([]);
+
+    const parseFileName = (files) => {
+        const ans = [];
+        for (let i = 0; i < Object.keys(files).length; i += 1) {
+            ans.push({ name: files[i].name });
+        }
+        setFileName(ans);
+    };
+
+    const showFile = async (e) => {
+        parseFileName(e.target.files);
+        e.preventDefault();
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            const fileBuffer = (e.target.result);
+            const temp = await decodeQR(fileBuffer)
+        }
+        reader.readAsArrayBuffer(e.target.files[0]);
+    };
+
+    var options = {
+        lineNumbers: true,
+    }
+
+    return (
+        <div style={{ margin: "2%" }}>
+            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+                <div>
+                    <div>
+                        <h1 className="font">Tx Params for Offline Signing</h1>
+                        <h4 style={{ textAlign: 'center' }}> OR</h4>
+                        <CodeMirror
+                            options={options}
+                            value=""
+                            height="200px"
+                            placeholder="Enter/Paste the raw data here"
+                            extensions={[javascript({ jsx: true })]}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <img id="qr" alt="qr" style={{ height: "400px", marginTop: 90 }} />
+                </div>
+            </div>
+
+            <div style={{ margin: 40, marginTop: 0 }}>
+                <label htmlFor="contained-button-file">
+                    <Input
+                        onChange={(e) => showFile(e)}
+                        accept="image/apng, image/avif, image/gif, image/jpeg, image/png, image/svg+xml, image/webp"
+                        id="contained-button-file" type="file" />
+                    <Button variant="contained" component="span">
+                        Upload File
+                    </Button>
+                </label>
+            </div>
+            {fileName.length > 0 ? <Typography variant="subtitle1">{`${fileName[0].name}`}</Typography> : null}
+
+        </div>
+
+    );
+}
+
+export default Read;
--- src/screens/qr/read/index.js
+++ src/screens/qr/read/index.js
@@ -1,10 +1,23 @@
 import { javascript } from '@codemirror/lang-javascript';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 import CodeMirror from '@uiw/react-codemirror';
 import QRCode from 'qrcode';
 import React, { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import { QrReader } from 'react-qr-reader';
+import decodeQR from '../../../utils/decodeQR';
 var beautify = require('js-beautify').js;
 
+const notify = (message) => toast.success(message, {
+    duration: 4000,
+    position: 'top-center',
+});
+
+const Input = styled('input')({
+    display: 'none',
+});
 
 const Reader = (props) => {
     const [data, setData] = useState('');
@@ -23,14 +36,39 @@ const Reader = (props) => {
             })
     }, [data])
 
+    const [fileName, setFileName] = React.useState([]);
+
+    const parseFileName = (files) => {
+        const ans = [];
+        for (let i = 0; i < Object.keys(files).length; i += 1) {
+            ans.push({ name: files[i].name });
+        }
+        setFileName(ans);
+    };
+
+    const showFile = async (e) => {
+        parseFileName(e.target.files);
+        e.preventDefault();
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            const fileBuffer = (e.target.result);
+            const temp = await decodeQR(fileBuffer)
+            console.log("temp", temp)
+            setData((temp))
+        }
+        reader.readAsArrayBuffer(e.target.files[0]);
+    };
+
     return (
         <div style={{ flex: 1, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <div style={{ width: '50%' }}>
                 <QrReader
-
+                    videoContainerStyle={{ margin: 40 }}
+                    scanDelay={5000}
                     onResult={(result, error) => {
                         if (!!result) {
                             setData(result?.text);
+                            notify('Scan Success')
                         }
 
                         if (!!error) {
@@ -38,18 +76,31 @@ const Reader = (props) => {
                         }
                     }}
                 />
+
+                <div style={{margin:40, marginTop:0}}>
+                    <label htmlFor="contained-button-file">
+                        <Input onChange={(e) => showFile(e)} accept="image/*" id="contained-button-file" type="file" />
+                        <Button variant="contained" component="span">
+                            Upload File
+                        </Button>
+                    </label>
+                    {fileName.length > 0 ? <Typography variant="subtitle1">{`${fileName[0].name}`}</Typography> : null}
+
+                </div>
+
             </div>
             <div style={{ width: '40%', }}>
                 <img id="qr" alt="qr" style={{ height: "400px", marginTop: 90 }} />
                 <CodeMirror
                     options={options}
-                    style={{ width: '100%'}}
+                    style={{ width: '100%' }}
                     value={beautify(data, { indent_size: 2, space_in_empty_paren: true })}
                     height="200px"
                     placeholder="Scan the QR and payload will be populated here"
                     extensions={[javascript({ jsx: true })]}
                 />
             </div>
+            <Toaster />
         </div>
     );
 };
